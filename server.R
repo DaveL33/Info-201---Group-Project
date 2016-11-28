89536cdf153a5306fdb0fa2bcce7ad388335f054
Juan Alvarez <juanalvarezdev@gmail.com>
2016-11-27 17:42:12 -0800

    Fixed date close #4

--- server.R
+++ server.R
@@ -1,9 +1,19 @@
 library(dplyr)
 library(shiny)
 
-runescape.data <- read.csv("~/Google Drive/College/Sophomore/Fall/INFO 201/Runescape_Market_Data.csv", stringsAsFactors = FALSE)
-View(runescape.data)
+#Import Runescape data from split files and turn to tables
+files <- list.files(path = "data/", full.names = TRUE)
+tables <- lapply(files, read.csv)
 
+#Merge 8 data files into one data frame
+runescape.data <- do.call(rbind, tables)
+
+#Fix PriceDate to make it readable and neat
+runescape.data <- runescape.data %>% mutate(PriceDate = as.POSIXct(as.numeric(as.character({runescape.data$PriceDate})), origin='1970-01-01', tz='GMT'))
+
+#Fix DateAdded to make it readable and neat
+runescape.data <- runescape.data %>% mutate(DateAdded = as.POSIXct(as.numeric(as.character({runescape.data$DateAdded})), origin='1970-01-01', tz='GMT'))
+  
 shinyServer(function(input, output) {
   
 })
