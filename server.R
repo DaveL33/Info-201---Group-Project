18f33596c059a7d9d0b210976c68fa67b7a0c3b5
David Lee <davel33@uw.edu>
2016-12-04 19:58:20 -0800

    simple graphics

--- server.R
+++ server.R
@@ -3,21 +3,20 @@ library(shiny)
 library(rsconnect)
 library(plotly)
 
+
 #Function that sets up runescape data from csv files
 initData <- function() {
-  
+
   #Import Runescape data from split files and turn to tables
   files <- list.files(path = "data/", full.names = TRUE)
-  tables <- lapply(files, read.csv)
+  tables <- lapply(files, read.csv, stringsAsFactors = FALSE)
   
   #Merge 8 data files into one data frame
   runescape.data <- do.call(rbind, tables)
-  
+  runescape.data$X.1 <- NULL
+  runescape.data$X <- NULL
   #Fix PriceDate to make it readable and neat
   runescape.data <- runescape.data %>% mutate(PriceDate = as.Date(as.POSIXct(as.POSIXct(runescape.data$PriceDate, origin="1970-01-01"), origin="1970-01-01")))
-  
-  #Fix DateAdded to make it readable and neat
-  runescape.data <- runescape.data %>% mutate(DateAdded = as.Date(as.POSIXct(as.POSIXct(runescape.data$DateAdded, origin="1970-01-01"), origin="1970-01-01")))
   
   #return data for scope
   return (runescape.data)
@@ -30,6 +29,7 @@ runescape.data <- initData()
 unique.category <- sort(as.vector(unique(runescape.data$Category)))
 
 shinyServer(function(input, output) {
+  
   #
   selected.category <- runescape.data
   
@@ -39,7 +39,7 @@ shinyServer(function(input, output) {
   output$itemSelect <- renderUI({ 
     
     #dataframe with filtered category is the category the user selected
-    selected.category <- selected.category %>% filter(Category == input$category)
+    selected.category <-  filter(runescape.data, Category == input$category)
     
     #Vector containing unique items for selected category 
     unique.item <- sort(as.vector(unique(selected.category$ItemName)))
@@ -53,11 +53,37 @@ shinyServer(function(input, output) {
     min.date <- min(selected.item$PriceDate, na.rm = TRUE)
     max.date <- max(selected.item$PriceDate, na.rm = TRUE)
     
-    sliderInput("date_range", 
+    sliderInput("dateSelect", 
                 "Choose Date Range:", 
                 min = min.date, max = max.date, 
                 timeFormat = "%F", 
                 value = c(min.date, max.date)
     )
+  })
+  
+  output$graphic <- renderPlotly({
+    #Dates, min and max, taken from the above input slider.
+    minDate <- min(input$dateSelect)
+    maxDate <- max(input$dateSelect)
+    
+    runescape.data <- filter(runescape.data, ItemName == input$item, PriceDate > minDate & PriceDate < maxDate) %>% 
+                      group_by(PriceDate) %>% 
+                      summarize(Gold = mean(Price))
+    
+    gold.plot <- plot_ly(runescape.data, x = ~PriceDate, y = ~Gold, name = "Price (GP)", type = "scatter", mode = 'lines') %>% 
+      layout(plot_bgcolor= 'rgba(193, 205, 205, 0.8)',
+             paper_bgcolor= 'rgba(193, 205, 205, 0.8)')
+    
+    return(gold.plot)
+  })
+  
+  output$ItemInfo <- renderTable({
+    runescape.data <- filter(runescape.data, ItemName == input$item)
+    Info <- c('Members Only', 'Low Alch', 'High Alch')
+    Data <- c(runescape.data$MembersOnly[[1]], runescape.data$LowAlch[[1]], 
+                runescape.data$HighAlch[[1]])
+    item.info <- data.frame(Info, Data)
+    
+    return(item.info)
   })
 })
--- ui.R
+++ ui.R
@@ -2,15 +2,22 @@ library(shiny)
 library(dplyr)
 library(plotly)
 library(rsconnect)
+library(shinythemes)
 
-source('server.R')
+#source('server.R')
 
-shinyUI(fluidPage(theme = "bootstrap.css",
+shinyUI(fluidPage(theme = shinytheme('slate'), style = "font-family: 'Century Gothic';
+                                                        font-size: 9pt;
+                                                        background-image: url('bg.jpg');
+                                                        background-position: center;
+                                                        background-size: cover",
   navbarPage("The Grand Exchange!",
+                
     tabPanel("Overview",
-             
+
+      
     # Header
-    titlePanel("Final Project Template"),
+    titlePanel(img(src='RunescapeLogo.png', width = '300px', height = '100px')),
     
     # Sidebar if necessary
     sidebarLayout(
@@ -21,10 +28,9 @@ shinyUI(fluidPage(theme = "bootstrap.css",
       mainPanel(
         tabsetPanel(
           tabPanel("Introduction",
-          img(src='RunescapeLogo.png'),
           h1("Introduction"),
-          p("Runescape is a free to play MMORPG (massively multiplayer online role playing game) that contains its own economy. In the game, players buy and sell virtual items for virtual gold coins through an in-game system called the Grand Exchange. Like a real economy, the price of a given item fluctuates with time and is dependent on a variety of ingame variables. The dataset we will be working with contains approximately three years worth of data that describes the price of all in-game items that are available to players. We got this dataset initially from Reddit, which was under a Github repository. Since Runescape has a low-level API, the user had to scrape the data from a website that had all of the prices for different items over the past years. This took approximately 20 hours using a scraping applications named BeautifulSoup and Requests. Our target audience would be Runescape users who want to get more information on their items, in order to get an edge while playing the game. The users are able to get a plethora of information from the scraped file. Given this 300MB csv file, the users are now able to ask: \"What would the price of a certain item be in the next month?\", \"How did the price change relative to the past three years?\", \"What items fluctuates more or less relative to other items?\", and \"Are there certain economic trends that can be used to predict the price in the distant future?\"")
-          ),
+          p("Runescape is a free to play MMORPG (massively multiplayer online role playing game) that contains its own economy. In the game, players buy and sell virtual items for virtual gold coins through an in-game system called the Grand Exchange. Like a real economy, the price of a given item fluctuates with time and is dependent on a variety of ingame variables. The dataset we will be working with contains approximately three years worth of data that describes the price of all in-game items that are available to players. We got this dataset initially from Reddit, which was under a Github repository. Since Runescape has a low-level API, the user had to scrape the data from a website that had all of the prices for different items over the past years. This took approximately 20 hours using a scraping applications named BeautifulSoup and Requests. Our target audience would be Runescape users who want to get more information on their items, in order to get an edge while playing the game. The users are able to get a plethora of information from the scraped file. Given this 300MB csv file, the users are now able to ask: \"What would the price of a certain item be in the next month?\", \"How did the price change relative to the past three years?\", \"What items fluctuates more or less relative to other items?\", and \"Are there certain economic trends that can be used to predict the price in the distant future?\""),
+          br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(), br(), br(), br(), br()),
           tabPanel("Technical Description",
           h1("Technical Description"),
           p("Our final product will be in the form of a Shiny app that allows users to enter the name of a specific in-game item and returns information about the history of that item in the Grand Exchange. We will be reading in a .csv file that contains the known history for all in-game items in Runescape, and what their prices were in the Grand Exchange for a given date, over about three years. We will also be leveraging the Runescape API which will allow us to obtain real time game information, such as the current price of an item in the Grand Exchange. Combining this .csv file and the game's API, we will be able to make comparisons as to how the current price of an item compares to previous prices, and make predictions on the future price of an item. Runescape players would find this information very valuable as it would give them strong insight into making lots of gold coins in the Runescape economy."),
@@ -33,10 +39,10 @@ shinyUI(fluidPage(theme = "bootstrap.css",
           br(),
           p("For this project, we will be learning to work with the Shiny framework for R. Since there are thousands of different items in Runescape, we will need an interactive web application that allows users to query information about specific in-game items, most likely via text entry. The Shiny app will allow users to ask questions about the data set, and we will also use Plotly to visualize information that we find about our data set, via charts and graphs."),
           br(),
-          p("The goal of this project is to use statistical analysis on the Runescape economy that would help players make in-game choices that would help them earn more gold coins. We anticipate that working with Runescape's API will be difficult, as in our research we have found reports that their API is not the easiest to work with. We also expect learning to use Shiny will be a challenge, as it is a new framework for everyone involved in the project.")
+          p("The goal of this project is to use statistical analysis on the Runescape economy that would help players make in-game choices that would help them earn more gold coins. We anticipate that working with Runescape's API will be difficult, as in our research we have found reports that their API is not the easiest to work with. We also expect learning to use Shiny will be a challenge, as it is a new framework for everyone involved in the project."),
+          br(),br(),br(),br(),br(),br(),br(),br())
         )
       )
-    )
     )
     ),
     
@@ -46,20 +52,37 @@ shinyUI(fluidPage(theme = "bootstrap.css",
     sidebarLayout(
       sidebarPanel(
         
-        selectInput("category", "Category:", unique.category, selected = unique.category[1], multiple = FALSE),
+        selectInput("category", label = h5("Category:"), unique.category, selected = unique.category[1], multiple = FALSE),
         
         uiOutput("itemSelect"),
 
-        uiOutput("dateSelect")
+        uiOutput("dateSelect"),
+        
+        h5("Item Information:"),
+        tableOutput('ItemInfo')
         
       ),
-      mainPanel("Main panel"
-                
+      mainPanel(h3("Simple Price Chart"),
+        p('Interactive line graph with descriptive stats based on gold, month, and year. Gives a basic overview of a chosen item\'s selling performance on the Grand Exchange over the last few years. Use the date slider to the left to reactively change the data based on a range of dates!'),
+        plotlyOutput('graphic'),
+        br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
+        br(),br(),br(),br(),br()
       )
     )
     
     ),
-    tabPanel("Predictive Price Chart"
+    tabPanel("Predictive Price Chart",
+    sidebarLayout(
+      sidebarPanel(
+        
+      ),
+      mainPanel(h3("Predictive Price Chart"),
+        p('Interactive plot that allows you to see real-time information about items and their stats pulled from RuneScape\'s API.'),
+        #plotlyOutput('graphic'),
+        br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
+        br(),br(),br(),br(),br()
+      )
+    )
              
     )
     
