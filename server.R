afd3a36c3abfc1bbb8893cf6d44dab55ebe939bf
David Lee <davel33@uw.edu>
2016-12-08 18:17:21 -0800

    Finalizing

--- server.R
+++ server.R
@@ -8,16 +8,22 @@ library(httr)
 
 #Function that sets up runescape data from csv files
 initData <- function() {
-  
   #Import Runescape data from split files and turn to tables
-  files <- list.files(path = "data/", pattern = "Runescape_Market_Data", full.names = TRUE)
+  files <-
+    list.files(path = "data/",
+               pattern = "Runescape_Market_Data",
+               full.names = TRUE)
   tables <- lapply(files, read.csv, stringsAsFactors = FALSE)
   
   #Merge 8 data files into one data frame
   runescape.data <- do.call(rbind, tables)
   
   #Fix PriceDate to make it readable and neat
-  runescape.data <- runescape.data %>% mutate(PriceDate = as.Date(as.POSIXct(as.POSIXct(runescape.data$PriceDate, origin = "1970-01-01"), origin = "1970-01-01")))
+  runescape.data <-
+    runescape.data %>% mutate(PriceDate = as.Date(as.POSIXct(
+      as.POSIXct(runescape.data$PriceDate, origin = "1970-01-01"),
+      origin = "1970-01-01"
+    )))
   
   #return data for scope
   return (runescape.data)
@@ -26,7 +32,6 @@ initData <- function() {
 # Runescape likes to frequently change the item codes that are used for making
 # API calls so this gets the most recent item codes from a 3rd party API
 initItemCodesData <- function() {
-  
   # Loads Runescape item data from API into a list (API has odd formatting)
   l <- RJSONIO::fromJSON('http://mooshe.pw/files/items_rs3.json')
   
@@ -53,79 +58,91 @@ item.codes <- initItemCodesData()
 unique.category <- sort(as.vector(unique(runescape.data$Category)))
 
 shinyServer(function(input, output) {
-  
   #Set category dataframe to be later modified and scoped
   selected.category <- runescape.data
   
   #Set item dataframe to be later modified and scoped
-  selected.item <- runescape.data 
+  selected.item <- runescape.data
   
   #Render itemSelect ui
-  output$itemSelect <- renderUI({ 
-    
+  output$itemSelect <- renderUI({
     #dataframe with filtered category is the category the user selected
-    selected.category <- filter(runescape.data, Category == input$category)
+    selected.category <-
+      filter(runescape.data, Category == input$category)
     
-    #Vector containing unique items for selected category 
-    unique.item <- sort(as.vector(unique(selected.category$ItemName)))
+    #Vector containing unique items for selected category
+    unique.item <-
+      sort(as.vector(unique(selected.category$ItemName)))
     
     #drop down to select item from category list
-    selectInput("item", "Item:", unique.item, selected = unique.item[1], multiple = FALSE)
+    selectInput("item",
+                "Item:",
+                unique.item,
+                selected = unique.item[1],
+                multiple = FALSE)
   })
   
   #Render dateSelect ui
   output$dateSelect <- renderUI({
-    
     #set up selected.item dataframe and get min and max values
-    selected.item <- selected.category %>% filter(ItemName == input$item)
+    selected.item <-
+      selected.category %>% filter(ItemName == input$item)
     min.date <- min(selected.item$PriceDate, na.rm = TRUE)
     max.date <- max(selected.item$PriceDate, na.rm = TRUE)
     
     #load slider input with dates
-    sliderInput("dateSelect", 
-                "Choose Date Range:", 
-                min = min.date, max = max.date, 
-                value = c(min.date, max.date)
+    sliderInput(
+      "dateSelect",
+      "Choose Date Range:",
+      min = min.date,
+      max = max.date,
+      value = c(min.date, max.date)
     )
   })
   
   #Render plot
   output$graphic <- renderPlotly({
-    
     #Dates, min and max, taken from the above input slider.
     min.date <- min(input$dateSelect)
     max.date <- max(input$dateSelect)
     
     #Using DPLYR functions, creates new column for gold values to be plotted.
-    plot.data <- runescape.data %>% 
-      filter(ItemName == input$item, PriceDate > min.date & PriceDate < max.date) %>% 
-      group_by(PriceDate) %>% 
+    plot.data <- runescape.data %>%
+      filter(ItemName == input$item,
+             PriceDate > min.date & PriceDate < max.date) %>%
+      group_by(PriceDate) %>%
       summarize(Gold = mean(Price))
     
     #Using Plotly, plots data from above created dataframe
-    plot_ly(plot.data,
-            x = ~PriceDate,
-            y = ~Gold,
-            type = "scatter",
-            mode = 'lines',
-            hoverinfo = 'text',
-            text = ~paste(Gold, 'GP')) %>% 
-      layout(title = "Price (GP) vs. Time",
-             plot_bgcolor= 'rgba(193, 205, 205, 0.8)', 
-             paper_bgcolor= 'rgba(193, 205, 205, 0.8)',
-             xaxis = list(title = "Date"))
+    plot_ly(
+      plot.data,
+      x = ~ PriceDate,
+      y = ~ Gold,
+      type = "scatter",
+      mode = 'lines',
+      hoverinfo = 'text',
+      text = ~ paste(Gold, 'GP')
+    ) %>%
+      layout(
+        title = "Price (GP) vs. Time",
+        plot_bgcolor = 'rgba(193, 205, 205, 0.8)',
+        paper_bgcolor = 'rgba(193, 205, 205, 0.8)',
+        xaxis = list(title = "Date")
+      )
   })
   
   #Renders the image of the item that the user is searching for information about. Gets
   #the image URL from the Runescape API
   output$ItemImage = renderUI({
-    
     # In case there is more than one item with the same item ID, get the first one
     # (the Runescape API says this is the best practice)
     item.id <- head(item.codes %>% filter(name == input$item), 1)
     
     # Get the image URL for the item icon based on its item id
-    image.url = paste0("http://services.runescape.com/m=itemdb_rs/1480946739712_obj_big.gif?id=", item.id$id)
+    image.url = paste0(
+      "http://services.runescape.com/m=itemdb_rs/1480946739712_obj_big.gif?id=",
+      item.id$id
+    )
     
     # Set the item icon as an image to be displayed in the app UI
     #Check if URL is valid
@@ -139,7 +156,8 @@ shinyServer(function(input, output) {
   
   #Render table under date slider
   output$ItemInfo <- renderTable({
-    base <- "http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item="
+    base <-
+      "http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item="
     
     # In case there is more than one item with the same item ID, get the first one
     # (the Runescape API says this is the best practice)
@@ -152,14 +170,33 @@ shinyServer(function(input, output) {
     item.data <- try(fromJSON(url))
     
     #If/Else statement for whether or not URL returns 404 error.
-    if(item.data[1] != "Error in open.connection(con, \"rb\") : HTTP error 404.\n"){
-      
+    if (item.data[1] != "Error in open.connection(con, \"rb\") : HTTP error 404.\n") {
       #make table data from runescape data
-      table.data <- runescape.data %>% filter(ItemName == input$item)
-      Info <- c('Description', 'Current Price (GP)', '% Change in Last 30 Days', '% Change in Last 90 Days', '% Change in Last 180 Days', 'Members Only', 'Low Alch', 'High Alch')
-      Data <- c(item.data[[1]]$description[[1]], item.data[[1]]$current$price, item.data[[1]]$day30$change, item.data[[1]]$day90$change, item.data[[1]]$day180$change, table.data$MembersOnly[[1]], table.data$LowAlch[[1]], table.data$HighAlch[[1]])
+      table.data <-
+        runescape.data %>% filter(ItemName == input$item)
+      Info <-
+        c(
+          'Description',
+          'Current Price (GP)',
+          '% Change in Last 30 Days',
+          '% Change in Last 90 Days',
+          '% Change in Last 180 Days',
+          'Members Only',
+          'Low Alch',
+          'High Alch'
+        )
+      Data <-
+        c(
+          item.data[[1]]$description[[1]],
+          item.data[[1]]$current$price,
+          item.data[[1]]$day30$change,
+          item.data[[1]]$day90$change,
+          item.data[[1]]$day180$change,
+          table.data$MembersOnly[[1]],
+          table.data$LowAlch[[1]],
+          table.data$HighAlch[[1]]
+        )
     } else {
-      
       #If URL is 404 error, return message.
       Info <- "Details N/A"
       Data <- "Item no longer in the Grand Exchange"
@@ -170,67 +207,140 @@ shinyServer(function(input, output) {
   })
   
   output$perCategory <- renderPlotly({
-    
-    #Initialize data frame to be used
-    categoryFrame <- data.frame()
-    
     #Bunch of if/elses for each radio button. Combines multiple similar categories into one.
-    if(input$checkCategory == 1){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Ammo", "Arrows", "Bolts", "Tools and containers", "Pocket items")) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
-    } else if (input$checkCategory == 2){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Construction materials", "Construction products", "Woodcutting product", "Mining and Smithing", "Familiars", "Fletching materials")) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
-    } else if (input$checkCategory == 3){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Farming produce", "Cooking ingredients", "Food and Drink", "Hunting Produce" )) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
-    } else if (input$checkCategory == 4){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Herblore materials", "Seeds", "Jewellery", "Prayer materials", "Potions")) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
-    } else if (input$checkCategory == 5){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Mage armour", "Melee armour - high level", "Melee armour - low level", "Melee armour - mid level", "Range armour", "Prayer armour")) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
-    } else if (input$checkCategory == 6){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Range weapons", "Mage weapons", "Hunting equipment", "Melee weapons - high level", "Melee weapons - low level", "Melee weapons - mid level" )) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
-    } else if (input$checkCategory == 7){
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Runecrafting", "Runes, Spells and Teleports", "Summoning scrolls", "Crafting materials", "Miscellaneous"  )) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
+    if (input$checkCategory == 1) {
+      combined.category <-
+        c("Ammo",
+          "Arrows",
+          "Bolts",
+          "Tools and containers",
+          "Pocket items")
+    } else if (input$checkCategory == 2) {
+      combined.category <-
+        c(
+          "Construction materials",
+          "Construction products",
+          "Woodcutting product",
+          "Mining and Smithing",
+          "Familiars",
+          "Fletching materials"
+        )
+    } else if (input$checkCategory == 3) {
+      combined.category <-
+        c("Farming produce",
+          "Cooking ingredients",
+          "Food and Drink",
+          "Hunting Produce")
+    } else if (input$checkCategory == 4) {
+      combined.category <-
+        c("Herblore materials",
+          "Seeds",
+          "Jewellery",
+          "Prayer materials",
+          "Potions")
+    } else if (input$checkCategory == 5) {
+      combined.category <-
+        c(
+          "Mage armour",
+          "Melee armour - high level",
+          "Melee armour - low level",
+          "Melee armour - mid level",
+          "Range armour",
+          "Prayer armour"
+        )
+    } else if (input$checkCategory == 6) {
+      combined.category <-
+        c(
+          "Range weapons",
+          "Mage weapons",
+          "Hunting equipment",
+          "Melee weapons - high level",
+          "Melee weapons - low level",
+          "Melee weapons - mid level"
+        )
+    } else if (input$checkCategory == 7) {
+      combined.category <-
+        c(
+          "Runecrafting",
+          "Runes, Spells and Teleports",
+          "Summoning scrolls",
+          "Crafting materials",
+          "Miscellaneous"
+        )
     } else if (input$checkCategory == 8) {
-      categoryFrame <- runescape.data %>% filter(Category %in% c("Costumes")) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
+      combined.category <- c("Costumes")
     } else {
-      categoryFrame <- runescape.data %>% filter(Category %in% c(unique.category)) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
+      combined.category <- c(unique.category)
     }
     
-    plot_ly(categoryFrame, x = ~PriceDate, y = ~Gold, name = 'Ammo', type = "scatter", mode = 'lines', line = list(color = 'rgb(205, 12, 24)'), hoverinfo = 'text', text = ~paste(Gold, 'GP')) %>% 
-      layout(title = "Average Price per Category", plot_bgcolor= 'rgba(193, 205, 205, 0.8)', paper_bgcolor= 'rgba(193, 205, 205, 0.8)')
+    
+    #Creating new filtered data frame per category
+    categoryFrame <-
+      runescape.data %>% filter(
+        Category %in% c(combined.category),
+        PriceDate > '2011-10-30',
+        PriceDate < '2015-06-20'
+      ) %>% group_by(PriceDate) %>% summarize(Gold = round(mean(Price)))
+    
+    #Plotting data for per category line graph
+    plot_ly(
+      categoryFrame,
+      x = ~ PriceDate,
+      y = ~ Gold,
+      name = 'Ammo',
+      type = "scatter",
+      mode = 'lines',
+      line = list(color = 'rgb(205, 12, 24)'),
+      hoverinfo = 'text',
+      text = ~ paste(Gold, 'GP')
+    ) %>%
+      layout(
+        title = "Average Price per Category",
+        plot_bgcolor = 'rgba(193, 205, 205, 0.8)',
+        paper_bgcolor = 'rgba(193, 205, 205, 0.8)'
+      )
     
   })
   
   #Render high vs low alch chart
   output$alchChart <- renderPlotly({
-    
     #Creates a new data frame with one high and one low alch for each item
-    y <- runescape.data[!duplicated(runescape.data$ItemName), c(3:5)]
+    y <-
+      runescape.data[!duplicated(runescape.data$ItemName), c(3:5)]
     
     #Removes outliers
-    alchData <- y %>% filter(LowAlch < 200000) %>% filter(HighAlch < 200000)
+    alchData <-
+      y %>% filter(LowAlch < 200000) %>% filter(HighAlch < 200000)
     
     #Using Plotly, plots high vs low alchemy chart
-    plot_ly(alchData, 
-            x = ~LowAlch, 
-            y = ~HighAlch, 
-            text = ~ItemName, 
-            type = "scatter", 
-            mode = "markers") %>% layout(plot_bgcolor= 'rgba(193, 205, 205, 0.8)',
-            paper_bgcolor= 'rgba(193, 205, 205, 0.8)',
-            xaxis = list(title = "Low Alchemy"),
-            yaxis = list(title = "High Alchemy"),
-            showlegend = FALSE)
+    plot_ly(
+      alchData,
+      x = ~ LowAlch,
+      y = ~ HighAlch,
+      text = ~ ItemName,
+      type = "scatter",
+      mode = "markers"
+    ) %>% layout(
+      plot_bgcolor = 'rgba(193, 205, 205, 0.8)',
+      paper_bgcolor = 'rgba(193, 205, 205, 0.8)',
+      xaxis = list(title = "Low Alchemy"),
+      yaxis = list(title = "High Alchemy"),
+      showlegend = FALSE
+    )
   })
   
   #Render the table for the category statistics
   output$categoryTable <- renderTable({
-    
     #Make data frame filtered per category
     stats <- runescape.data %>% filter(Category == input$statCat)
     
     #Assign info and data for max item
-    Info <- c("Item Name", "Most Expensive Price", "Date Occurrence")
-    Data <- c(stats$ItemName[stats$Price == max(stats$Price, na.rm = TRUE)][1], paste0(unique(stats$Price[stats$Price == max(stats$Price, na.rm = TRUE)]), " GP"), as.Date(stats$PriceDate[stats$Price == max(stats$Price, na.rm = TRUE)][1]))
+    Info <-
+      c("Item Name", "Most Expensive Price", "Date Occurrence")
+    Data <-
+      c(stats$ItemName[stats$Price == max(stats$Price, na.rm = TRUE)][1],
+        paste0(unique(stats$Price[stats$Price == max(stats$Price, na.rm = TRUE)]), " GP"),
+        as.Date(stats$PriceDate[stats$Price == max(stats$Price, na.rm = TRUE)][1]))
     
     #Return data frame with format data
     return(data.frame(Info, Data))
